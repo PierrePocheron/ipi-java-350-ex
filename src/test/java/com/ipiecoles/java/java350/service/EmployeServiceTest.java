034b683fa27868da868c35c754d0901b3437f185
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-10 11:00:29 +0100

    Add Service test

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceTest.java
@@ -9,6 +9,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -107,4 +108,32 @@ class EmployeServiceTest {
             Assertions.assertThat(e.getMessage()).isEqualTo("L'employé de matricule T00001 existe déjà en BDD");
         }
     }
+
+//    @Test
+//    public void testEmbauchePremierEmploye() throws EmployeException {
+//        //Given Pas d'employés en base
+//        String nom = "Doe";
+//        String prenom = "John";
+//        Poste poste = Poste.TECHNICIEN;
+//        NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
+//        Double tempsPartiel = 1.0;
+//        //Simuler qu'aucun employé n'est présent (ou du moins aucun matricule)
+//        Mockito.when(employeRepository.findLastMatricule()).thenReturn(null);
+//        //Simuler que la recherche par matricule ne renvoie pas de résultats
+//        Mockito.when(employeRepository.findByMatricule("T00001")).thenReturn(null);
+//        //When
+//        employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
+//        //Then
+//        ArgumentCaptor<Employe> employeArgumentCaptor = ArgumentCaptor.forClass(Employe.class);
+////        Mockito.verify(employeRepository, Mockito.times(1)).save(employeArgumentCaptor.capture());
+//        Mockito.verify(employeRepository).save(employeArgumentCaptor.capture());
+//        Employe employe = employeArgumentCaptor.getValue();
+//        Assertions.assertThat(employe).isNotNull();
+//        Assertions.assertThat(employe.getNom()).isEqualTo(nom);
+//        Assertions.assertThat(employe.getPrenom()).isEqualTo(prenom);
+//        Assertions.assertThat(employe.getSalaire()).isEqualTo(1825.46);
+//        Assertions.assertThat(employe.getTempsPartiel()).isEqualTo(1.0);
+//        Assertions.assertThat(employe.getDateEmbauche()).isEqualTo(LocalDate.now());
+//        Assertions.assertThat(employe.getMatricule()).isEqualTo("T00001");
+//    }
 }
