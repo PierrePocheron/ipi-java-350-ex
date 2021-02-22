427da9812a7314aab823b30fd77b46d2e637ce6d
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-22 16:55:10 +0100

    remove code smells

--- src/main/java/com/ipiecoles/java/java350/Java350Application.java
+++ src/main/java/com/ipiecoles/java/java350/Java350Application.java
@@ -3,8 +3,6 @@ package com.ipiecoles.java.java350;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-
 @SpringBootApplication
 public class Java350Application {
 
