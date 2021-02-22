cc2292c5f6c6aa3c4d061e7cb786158985939f79
PierrePocheron <pierre.pocheron@gmail.com>
2021-02-22 17:42:54 +0100

    remove code smells

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -58,7 +58,7 @@ public class EmployeService {
         matricule = typeEmploye + matricule.substring(matricule.length() - 5);
         //On vérifie l'existence d'un employé avec ce matricule
         if(employeRepository.findByMatricule(matricule) != null){
-            logger.error("L'employé de matricule " + matricule + " existe déjà en BDD");
+            logger.error("L'employé de matricule existe déjà en BDD");
             throw new EntityExistsException("L'employé de matricule " + matricule + " existe déjà en BDD");
         }
 
